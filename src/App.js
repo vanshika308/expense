dafa3e43332f54fb2c9321cd911b3995c24b45f6
vanshika308 <vanshikatiwari0308@gmail.com>
2023-07-01 00:58:46 +0530

    rendering expenses with performance optimization

--- src/App.js
+++ src/App.js
@@ -3,40 +3,44 @@ import React,{useState} from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+const DUMMY_EXPENSES=[
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: 'Bangalore',
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    location: 'Delhi',
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location: 'Jaipur',
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location: 'Noida',
+  },
+];
 function App() {
-  const [expenses,setExpenses] = useState([
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: 'Bangalore',
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      location: 'Delhi',
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: 'Jaipur',
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: 'Noida',
-    },
-  ]);
+ 
+  const[expenses,setExpenses]=useState(DUMMY_EXPENSES);
   const addExpenseHandler = expense =>{
-    setExpenses((prevExpenses)=>[expense,...prevExpenses]);
-  }
+    setExpenses((prevExpenses)=>{
+      return[expense,...prevExpenses];
+    });
+  };
 
   return (
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,30 +13,14 @@ function Expenses(props){
     return (
         <Card className="expenses">
          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-          <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
-            location={props.items[0].location}
-          />
-          <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}
-            location={props.items[1].location}
-          />
-          <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-            location={props.items[2].location}
-          />
-          <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-            location={props.items[3].location}
-          />
+          {props.items.map(expense =>
+          <ExpenseItem 
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+          />)}
         </Card>
       );
 }
